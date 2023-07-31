f0e785763f573128d941d0efa5cadbe9b8f22758
Kaung Htet Naing <kaunghtetnaing@Kaungs-MacBook-Pro.local>
2023-07-31 19:38:33 +0700

    build fix

--- app/registry.tsx
+++ app/registry.tsx
@@ -11,7 +11,9 @@ export default function StyledComponentsRegistry({
 }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
+  const [styledComponentsStyleSheet]: any = useState(
+    () => new ServerStyleSheet()
+  )
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement()
